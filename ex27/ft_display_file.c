a73cb14712ebf944f0aebd1e6578f639316f4071
Anthony Grimaldo Ramos <agrimald@car5s6.42barcelona.com>
2023-05-01 20:33:54 +0200

    corrigiendo el ex27

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,39 +6,12 @@
 /*   By: agrimald <agrimald@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 17:43:58 by agrimald          #+#    #+#             */
-/*   Updated: 2023/05/01 19:09:15 by agrimald         ###   ########.fr       */
+/*   Updated: 2023/05/01 20:32:33 by agrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-/*int main()
-{
-	FILE *archivo;
-	char name;
-	
-	archivo=fopen("archivo.txt", "r");
-	if(archivo== NULL)
-	{
-		printf("Error al abrir el archivo\n");
-	}
-	else
-	{
-		printf("\n El contenido del archivo es:\n\n");
-		while(feof(archivo)==0)
-		{
-			name=fgetc(archivo);
-			printf("%c", name);
-		}
-	}
-	fclose(archivo);
-	getch();
-	return 0;
-}
-*/
-#include <stdio.h>
-
+/*#include <stdio.h>
+//estaba hecho pero faltaban mas cositas
 int main()
 {
     FILE *archivo;      // Declarar un puntero a un objeto de tipo FILE
@@ -60,4 +33,87 @@ int main()
     }
     return 0;   // Indicar que el programa finalizó correctamente
 }
+*/
+//estudiar esto
+#include <stdio.h>
 
+#define BUFFER_SIZE 4096
+
+int main(int argc, char *argv[]) {
+    // Verificar el número de argumentos
+    if (argc != 2)
+   	{
+        if (argc < 2)
+	   	{
+            // Si no se proporciona un nombre de archivo, mostrar un mensaje de error
+            printf("File name missing.\n");
+        } else
+	   	{
+            // Si se proporcionan demasiados argumentos, mostrar otro mensaje de error
+            printf("Too many arguments.\n");
+        }
+        return 1; // Salir del programa con un código de error
+    }
+
+    // Abrir el archivo en modo de lectura
+    FILE *archivo = fopen(argv[1], "r");
+    if (archivo == NULL) 
+	{
+        // Si el archivo no se puede abrir, mostrar un mensaje de error
+        printf("Cannot read file.\n");
+        return 1; // Salir del programa con un código de error
+    }
+
+    // Leer el contenido del archivo y mostrarlo en la salida estándar
+    char buffer[BUFFER_SIZE];
+    int num_leidos;
+    while ((num_leidos = fread(buffer, 1, BUFFER_SIZE, archivo)) > 0) {
+        // Leer el archivo en bloques de tamaño fijo y mostrarlos en la salida estándar
+        fwrite(buffer, 1, num_leidos, stdout);
+    }
+
+    // Cerrar el archivo
+    fclose(archivo);
+
+    return 0;
+}
+/*
+ * otra manera de hacerlo
+#include <stdio.h>
+
+#define BUFFER_SIZE 4096 // Tamaño máximo del buffer de lectura
+
+int main()
+{
+    char filename[256]; // Buffer para almacenar el nombre del archivo
+    FILE *file;
+    char buffer[BUFFER_SIZE];
+    size_t bytes_read;
+
+    printf("Ingrese el nombre del archivo: ");
+    scanf("%255s", filename); // Lee hasta 255 caracteres desde la entrada estándar
+
+    file = fopen(filename, "r");
+    if (!file)
+    {
+        printf("No se pudo abrir el archivo %s.\n", filename);
+        return 1;
+    }
+
+    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, file)) > 0)
+    {
+        fwrite(buffer, 1, bytes_read, stdout);
+    }
+
+    if (ferror(file))
+    {
+        printf("No se pudo leer el archivo %s.\n", filename);
+        fclose(file); // Cerramos el archivo antes de salir del programa
+        return 1;
+    }
+
+    fclose(file);
+
+    return 0;
+}
+*/
